560ae64e474a83449a6010f467f7ce616eb3024c
yccc233 <1099312164@qq.com>
2021-10-28 15:38:20 +0800

    数据库改名

--- server/datas/db.js
+++ server/datas/db.js
@@ -1,5 +1,10 @@
+/**
+ * 操作数据库的，也就是建设数据库结构和管理等，也方便数据调整
+ * 2021年10月28日 15:23:13
+ */
+
 var sqlite3 = require("sqlite3").verbose();
-var database = new sqlite3.Database("./express-info.db", function(e){
+var database = new sqlite3.Database("./database.db", function(e){
     if (e) throw e;
 });
 
@@ -7,15 +12,14 @@ var database = new sqlite3.Database("./express-info.db", function(e){
 /**
  * 创建user_account表
  */
-var createTable = "create table user_account (" +
-    "id integer auto_increment," +
+var createTableSQL = "create table user_account (" +
+    "id integer primary key," +
     "account varchar(10) not null," +
     "password varchar(20) not null," +
-    "role varchar(1) not null default 0," +
-    "userid integer primary key" +
+    "role varchar(1) not null" +
     ");"
 
+console.log(createTableSQL)
 
-
-database.run(createTable);
-setTimeout(() => database.close(), 1000);
+database.run(createTableSQL);
+setTimeout(() => database.close(), 100);
